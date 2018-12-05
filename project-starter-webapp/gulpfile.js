2f64b9ce2d540ee0337e727f105d4a0e4b9d612b
Bryan Bende <bbende@gmail.com>
2018-12-05 18:29:51 -0500

    Setting up gulp task to bundle javascript with browserify

--- project-starter-webapp/gulpfile.js
+++ project-starter-webapp/gulpfile.js
@@ -5,22 +5,32 @@ var minify = require('gulp-minify');
 var cleanCss = require('gulp-clean-css');
 var sass = require('gulp-sass');
 var util = require('gulp-util');
+var uglify = require('gulp-uglify');
 var tildeImporter = require('node-sass-tilde-importer');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
 
-// Copy resources to the buildDir and use that as the source for other tasks
+var srcDir = util.env.srcDir;
+var buildDir = util.env.buildDir;
+var outDir = util.env.outDir;
+var minifyFlag = util.env.envName === 'prod';
+
+// Copy stylesheets to the buildDir and use that as the source for other tasks
 gulp.task('copy-stylesheets', function () {
-    var srcDir = util.env.srcDir;
-    var buildDir = util.env.buildDir;
-
     return gulp.src(srcDir + '/stylesheets/**/*')
         .pipe(gulp.dest(buildDir + '/stylesheets'));
+});
+
+// Copy stylesheets to the buildDir and use that as the source for other tasks
+gulp.task('copy-javascript', function () {
+    return gulp.src(srcDir + '/javascript/**/*')
+        .pipe(gulp.dest(buildDir + '/javascript'));
 });
 
 // The tilde-importer is required to resolve imports like ~bootstrap/scss/...
 // The includePaths avoids the need to prefix all imports with the path to node_modules
 gulp.task('compile-sass', ['copy-stylesheets'], function() {
-    var buildDir = util.env.buildDir;
-
     return gulp.src(buildDir + '/stylesheets/**/*.scss')
         .pipe(sass({
             includePaths: [ './node_modules' ],
@@ -31,30 +41,28 @@ gulp.task('compile-sass', ['copy-stylesheets'], function() {
 
 // Pack any .css files in the buildDir into application.css in outDir/stylesheets
 gulp.task('pack-css', ['compile-sass'] , function () {
-    var buildDir = util.env.buildDir;
-    var outDir = util.env.outDir;
-    var minifyFlag = util.env.envName === 'prod';
-
     return gulp.src(buildDir + '/stylesheets/**/*.css')
         .pipe(concat('application.css'))
         .pipe(gulpif(minifyFlag, cleanCss()))
         .pipe(gulp.dest(outDir + '/stylesheets'));
 });
 
-gulp.task('copy-external-javascript', function () {
-    var srcDir = util.env.srcDir;
-    var buildDir = util.env.buildDir;
-
-    return gulp.src(srcDir + '/stylesheets/**/*')
-        .pipe(gulp.dest(buildDir + '/stylesheets'));
+// Creates a bundle for application.js and conditionally calls minify based on dev or prod
+gulp.task('bundle-javascript', ['copy-javascript'], function() {
+    return browserify({ entries: [buildDir + '/javascript/application.js'] })
+        .bundle()
+        .pipe(source('application.js'))
+        .pipe(buffer())
+        .pipe(gulpif(minifyFlag, uglify()))
+        .pipe(gulp.dest(outDir + '/javascript'));
 });
 
-gulp.task('default',
-    [
-        'pack-css'
-    ]);
+gulp.task('default', ['pack-css', 'bundle-javascript']);
 
+// Watches for changes and executes appropriate targets so we don't have to rebuild during development
 gulp.task('watch', function() {
-    var srcDir = util.env.srcDir;
-    gulp.watch([srcDir + '/stylesheets/**/*'], ['pack-css']);
+    gulp.watch([
+        srcDir + '/stylesheets/**/*',
+        srcDir + '/javascript/**/*'
+    ], ['pack-css', 'bundle-javascript']);
 });
--- project-starter-webapp/src/main/webapp/javascript/application.js
+++ project-starter-webapp/src/main/webapp/javascript/application.js
@@ -1,5 +1,20 @@
-import 'bootstrap';
-import "rails-ujs";
-import Turbolinks from "turbolinks";
+require('bootstrap');
+require("rails-ujs");
 
+let Turbolinks = require("turbolinks");
 Turbolinks.start();
+
+let $ = require('jquery')
+
+$(document).ready(function() {
+    // Toggle navbarSide when button is clicked
+    $('#navbarSideButton').on('click', function() {
+        if ( $('#navbarSide').hasClass('navbar-side-reveal') ) {
+            $('#navbarSide').removeClass('navbar-side-reveal');
+            $('main').removeClass('push-main');
+        } else {
+            $('#navbarSide').addClass('navbar-side-reveal');
+            $('main').addClass('push-main');
+        }
+    });
+});
