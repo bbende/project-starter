2c53a7b6925be4447e24c27d154547048463f40a
Bryan Bende <bbende@gmail.com>
2020-05-21 14:16:42 -0400

    Add font-awesome and use font-awesome icons on buttons

--- project-starter-webapp/gulpfile.js
+++ project-starter-webapp/gulpfile.js
@@ -19,7 +19,8 @@ var minifyFlag = argv.envName === 'dist';
 // Copy stylesheets to the buildDir and use that as the source for other tasks
 gulp.task('copy-stylesheets', function () {
     return gulp.src([
-            srcDir + '/stylesheets/**/*'
+            srcDir + '/stylesheets/**/*',
+            'node_modules/font-awesome/css/font-awesome.css'
         ])
         .pipe(gulp.dest(buildDir + '/stylesheets'));
 });
@@ -31,12 +32,12 @@ gulp.task('copy-javascript', function () {
 });
 
 // Copy fonts to the outDir, we skip buildDir since there is nothing to do to the fonts
-//gulp.task('copy-fonts', function () {
-//    return gulp.src([
-//        'node_modules/bootstrap-material-design-icons/fonts/*'
-//      ])
-//        .pipe(gulp.dest(outDir + '/fonts'));
-//});
+gulp.task('copy-fonts', function () {
+    return gulp.src([
+        'node_modules/font-awesome/fonts/*'
+      ])
+        .pipe(gulp.dest(outDir + '/fonts'));
+});
 
 // The tilde-importer is required to resolve imports like ~bootstrap/scss/...
 // The includePaths avoids the need to prefix all imports with the path to node_modules
@@ -69,7 +70,7 @@ gulp.task('bundle-javascript', gulp.series('copy-javascript', function() {
         .pipe(gulp.dest(outDir + '/javascript'));
 }));
 
-gulp.task('default', gulp.parallel('bundle-css', 'bundle-javascript'));
+gulp.task('default', gulp.parallel('copy-fonts', 'bundle-css', 'bundle-javascript'));
 
 // Watches for changes and executes appropriate targets so we don't have to rebuild during development
 gulp.task('watch', function() {
