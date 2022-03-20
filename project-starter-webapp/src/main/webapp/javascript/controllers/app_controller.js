c63b879c82f1632cfb7ded4079613afcb20db79b
Bryan Bende <bbende@gmail.com>
2022-03-20 14:26:44 -0400

    Initial refactor - create bootstrap module, remove stimulus, switch to webjars, remove gulp

--- project-starter-webapp/src/main/webapp/javascript/controllers/app_controller.js
+++
@@ -1,9 +0,0 @@
-import { Controller } from "@hotwired/stimulus";
-
-export default class extends Controller {
-
-    initialize() {
-        // App controller is part of the overall layout
-    }
-
-}
--- project-starter-webapp/src/main/webapp/javascript/load-jquery.js
+++
@@ -1,5 +0,0 @@
-// Exposes jQuery and Popper to other libraries like bootstrap-material
-// Must be first import in application.js, see - https://stackoverflow.com/a/51271892
-
-window.$ = window.jQuery = require('jquery');
-window.Popper = require('popper.js');
