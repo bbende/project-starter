79aa74786192c1097baccde21c03e7977e7b6618
Bryan Bende <bbende@gmail.com>
2022-07-13 21:50:58 -0400

    Clean up imports

--- project-starter-webapp/src/main/java/com/bbende/project/starter/security/SecurityConfig.java
+++ project-starter-webapp/src/main/java/com/bbende/project/starter/security/SecurityConfig.java
@@ -1,19 +1,14 @@
 package com.bbende.project.starter.security;
 
-import com.bbende.project.starter.security.cookie.CookieService;
 import com.bbende.project.starter.security.token.TokenAuthenticationFilter;
 import com.bbende.project.starter.security.token.TokenAuthenticationSuccessHandler;
-import com.bbende.project.starter.security.token.TokenService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
